185755551eca6a15d0a74f7a5652a143a5df3cd5
voskpaul <paulrieii@gmail.com>
2024-04-17 21:42:05 +0000

    README.md

---
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+import './Notifications.css';
+
+export default function Notifications() {
+	return (
+		<div className="Notifications">
+			<p>
+				Here is the list of notifications
+			</p>
+		</div>
+	);
+};
+
