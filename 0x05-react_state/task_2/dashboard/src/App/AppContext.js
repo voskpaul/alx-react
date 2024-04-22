18d640d4d9ebdcaf8bfc0a22859509e988a3d74e
voskpaul <paulriemii@gmail.com>
2024-04-22 18:11:00 +0000

    react_state

---
+++ 0x05-react_state/task_2/dashboard/src/App/AppContext.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+export const user = {
+  email: '',
+  password: '',
+  isLoggedIn: false
+};
+
+export function logOut() {
+  user.isLoggedIn = false;
+}
+
+export const AppContext = React.createContext({
+	user,
+	logOut
+});
+
---
+++ 0x05-react_state/task_4/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,3 @@
+
+NotificationItemShape.js 
+
